b192cfb71aa1144368f25835ce3927f84043316e
claradeuve <claradvillegas@gmail.com>
2022-12-09 18:59:20 +0100

    Primer test

--- src/main.py
+++ src/main.py
@@ -1,8 +1,26 @@
+from unittest.mock import patch
 from bot import WhatsappBot
+import unittest
+
+class TestConsumer(unittest.TestCase):
+    @patch('bot.WhatsappBot._get_intent')
+    def test_my_bot_2(self, intent):
+        my_bot_2 = WhatsappBot(language="es")
+        expected_resp = {
+                    "answer": {
+                        "id": 0,
+                        "message": "Perfecto, hemos guardado tu e-mail! Disfruta de la experiencia en el restaurante.",
+                    },
+                    "action": "hangup"
+                }
+        resp = my_bot_2.message("no quiero recibir nada", "ask_for_email")
+        self.assertEquals(expected_resp, resp, f"Unexpected response")
+
 
 
 if __name__ == "__main__":
 
+    unittest.main()
     my_bot = WhatsappBot(language="en")  # Bot should re instantiated for each conversation
     my_bot_2 = WhatsappBot(language="es")
     my_bot_3 = WhatsappBot(language="en")
@@ -34,4 +52,6 @@ if __name__ == "__main__":
 
     # Conv 3
     my_bot_3.message("Yes, I'd love to!", "newsletter")
-    my_bot_3.message("I prefer not to share my email", "newsletter")
+    my_bot_3.message("I prefer to get by notifications by post")
+    my_bot_3.message("My address is 742 Evergreen Terrace")
+    my_bot_3.message("I don't want emails")
