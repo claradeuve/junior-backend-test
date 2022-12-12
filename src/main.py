8949a6dff52b374508535c056ff29f1d25ddcf04
claradeuve <claradvillegas@gmail.com>
2022-12-12 19:56:31 +0100

    Incluir variante adicional al inicio de conversación con "confirm"

--- src/main.py
+++ src/main.py
@@ -196,12 +196,19 @@ if __name__ == "__main__":
     my_bot.message("No, I would hate to receive notifications", "newsletter")
 
     # Conv 3
-    my_bot.message("My email is info@bookline.io", "ask_for_email")
+    my_bot.message("Which kind of notifications would I get?", "newsletter")
+    # Alternativa al inicio de conversación de tipo 'newsletter'. El intent no quedaría determinado,
+    # y se volvería a instar al usuario a dar una respuesta de 'sí' o 'no'.
+    # Si contestara 'yes' (u otra respuesta con intent afirmativo), la conversación continuaría y seguiría el mismo itinerario que 'Conv 1'
+    # Si contesara 'no' (u otra respuesta con intent negativo), la conversación finalizaría, al igual que en el caso de 'Conv 2'
 
     # Conv 4
-    my_bot.message("My email is not_a_valid_email.com", "ask_for_email")
+    my_bot.message("My email is info@bookline.io", "ask_for_email")
 
     # Conv 5
+    my_bot.message("My email is not_a_valid_email.com", "ask_for_email")
+
+    # Conv 6
     my_bot.message("This should not happen, so bot should hangup", "ask_for_card")
 
     # Example of a full conversation
